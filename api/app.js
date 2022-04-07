471b9bb59c2a40e7366162a51560ad57e1a4ef32
JuhoLaurila00 <juho.laurila00@gmail.com>
2022-04-07 19:00:03 +0300

    Webtoken

--- api/app.js
+++ api/app.js
@@ -4,13 +4,16 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 const asiakasRouter = require('./routes/asiakas');
 const asiakas_has_tiliRouter = require('./routes/asiakas_has_tili');
 const korttiRouter = require('./routes/kortti');
 const tiliRouter = require('./routes/tili');
 const tilitapahtumatRouter = require('./routes/tilitapahtumat');
-const loginRouter = require('./routes/login');;
+const loginRouter = require('./routes/login');
+
+
 
 var app = express();
 
@@ -24,9 +27,28 @@ app.use(cors());
 
 app.use('/asiakas', asiakasRouter);
 app.use('/asiakas_has_tili', asiakas_has_tiliRouter);
-app.use('/kortti', korttiRouter);
 app.use('/tili', tiliRouter);
 app.use('/tilitapahtumat', tilitapahtumatRouter);
 app.use('/login', loginRouter);
+app.use(authenticateToken);
+app.use('/kortti', korttiRouter);
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+  
+    console.log("token = "+token);
+    if (token == null) return res.sendStatus(401)
+  
+    jwt.verify(token, process.env.MY_TOKEN, (err, user) => {
+      console.log(err)
+  
+      if (err) return res.sendStatus(403)
+  
+      req.user = user
+  
+      next()
+    })
+  }
 
 module.exports = app;
