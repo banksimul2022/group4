86b33c34455cd34df22004daddeea52a96d3ee74
VenniKarhukorpi <t1kave01@students.oamk.fi>
2022-04-13 15:13:55 +0300

    Dll tiedostot luotu

---
+++ bankautomat/DLLRestAPI/DLLRestAPI/dllrestapi.cpp
@@ -0,0 +1,5 @@
+#include "dllrestapi.h"
+
+DLLRestAPI::DLLRestAPI()
+{
+}
---
+++ bankautomat/DLLRestAPI/DLLRestAPI/dllrestapi.h
@@ -0,0 +1,12 @@
+#ifndef DLLRESTAPI_H
+#define DLLRESTAPI_H
+
+#include "DLLRestAPI_global.h"
+
+class DLLRESTAPI_EXPORT DLLRestAPI
+{
+public:
+    DLLRestAPI();
+};
+
+#endif // DLLRESTAPI_H
